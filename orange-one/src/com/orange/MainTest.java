b22c5f065ef62c3a9c5be983031bfc7e23d0a4e5
xinghewanli <2088451786@qq.com>
2022-01-23 22:53:42 +0200

    回退测试

--- orange-one/src/com/orange/MainTest.java
+++ orange-one/src/com/orange/MainTest.java
@@ -3,6 +3,6 @@ package com.orange;
 public class MainTest {
  public static void main(String[] args){
      System.out.println("aaaaaa");
-     System.out.println("test change");
+     System.out.println("test change ");
  }
 }
